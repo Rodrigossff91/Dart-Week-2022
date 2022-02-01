657da2ea583de2b07f8e3df75ea02336ed0b0352
Rodrigo Ferreira <rodrigo@treeinova.com.br>
2022-02-01 01:05:38 -0300

    Dart Week app Vakinha

---
+++ vakinha_burgue/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burgue/app/routes/splash_routers.dart';
+
+void main() {
+  runApp(const VakinhaBurgueMainApp());
+}
+
+class VakinhaBurgueMainApp extends StatelessWidget {
+  const VakinhaBurgueMainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Vakinha Burguer',
+      getPages: [
+        ...SplashRouters.routers,
+      ],
+    );
+  }
+}
